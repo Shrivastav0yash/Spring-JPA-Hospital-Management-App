a44b49d755eed654a84565b57e9cbb3db7f88ad8
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-20 11:45:48 +0530

    Initial commit - Spring JPA Hospital Management App

---
+++ src/main/java/com/springjpa/hospital_management_app/HospitalManagementApp.java
@@ -0,0 +1,13 @@
+package com.springjpa.hospital_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/springjpa/hospital_management_app/Chapter8SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpa.hospital_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter8SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
