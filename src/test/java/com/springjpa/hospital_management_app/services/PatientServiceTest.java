4bac4f8b67e349ee42d3f66fc49427d292d99da3
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-22 14:45:07 +0530

    git commit -m "test some more cases on patient "

--- src/test/java/com/springjpa/hospital_management_app/services/PatientServiceTest.java
+++ src/test/java/com/springjpa/hospital_management_app/services/PatientServiceTest.java
@@ -1,9 +1,13 @@
 package com.springjpa.hospital_management_app.services;
 
 import com.springjpa.hospital_management_app.entity.Patient;
+import com.springjpa.hospital_management_app.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 public class PatientServiceTest {
@@ -12,9 +16,35 @@ public class PatientServiceTest {
     @Autowired
     private PatientService patientService;
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @Test
     public void testTransactionMethods(){
+
+        /*
         Patient patient = patientService.getPatientById(1L);
         System.out.println(patient);
+         */
+        /*
+
+        Patient patient  = patientRepository.findByName("Neha Gupta");
+        System.out.println(patient);
+
+        List<Patient> patientsList = patientRepository.findByBirthDateOrEmail(
+                LocalDate.of(2001, 3, 18),
+                "rahul.sharma@gmail.com"
+        );
+
+        for(Patient i : patientsList){
+            System.out.println(i);
+        }
+
+         */
+        List<Patient> patientsList = patientRepository.findByNameContainingOrderByIdDesc("n");
+
+        for(Patient i : patientsList){
+            System.out.println(i);
+        }
     }
 }
