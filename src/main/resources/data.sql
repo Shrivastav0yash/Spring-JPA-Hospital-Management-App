e97a72473c3d0b62551eb769a31cbd45a10e759b
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-22 14:43:50 +0530

    git commit -m "Add default sql file"

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO patient (name, birth_date, email, gender, blood_group)
+VALUES
+('Rahul Sharma', '1998-05-12', 'rahul.sharma@gmail.com', 'Male', 'A_POSITIVE'),
+('Priya Verma', '2000-08-22', 'priya.verma@gmail.com', 'Female', 'B_POSITIVE'),
+('Amit Singh', '1995-12-10', 'amit.singh@gmail.com', 'Male', 'O_POSITIVE'),
+('Neha Gupta', '2001-03-18', 'neha.gupta@gmail.com', 'Female', 'AB_POSITIVE'),
+('Karan Mehta', '1997-07-25', 'karan.mehta@gmail.com', 'Male', 'A_NEGATIVE');
