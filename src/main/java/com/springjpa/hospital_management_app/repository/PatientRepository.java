ee1a899e74d5439130811c76615c7ba7245bf73c
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-22 14:44:39 +0530

    git commit -m "Add new query method in repo"

--- src/main/java/com/springjpa/hospital_management_app/repository/PatientRepository.java
+++ src/main/java/com/springjpa/hospital_management_app/repository/PatientRepository.java
@@ -4,7 +4,14 @@ import com.springjpa.hospital_management_app.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Patient findByName(String name);
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+    List<Patient>  findByNameContainingOrderByIdDesc(String name);
 
 }
