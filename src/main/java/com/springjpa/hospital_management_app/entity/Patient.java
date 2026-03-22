706a72d8f5fd5abf37cb88fda325f7ac9f18aad6
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-22 14:44:14 +0530

    git commit -m "Add constraints in patient entity"

--- src/main/java/com/springjpa/hospital_management_app/entity/Patient.java
+++ src/main/java/com/springjpa/hospital_management_app/entity/Patient.java
@@ -1,24 +1,48 @@
 package com.springjpa.hospital_management_app.entity;
 
+import com.springjpa.hospital_management_app.entity.type.BloodGroupType;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table (
+        name = "patient",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_patient_name_birthDate", columnNames = {"name", "birthDate"})
+                //when unique constraint created it always check these condition is valid at the time of insertion so insert query becomes
+                //slow
+        },
+        indexes = {
+                @Index(name = "idx_patient_birthDate", columnList = "birthDate")
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String name;
 
     private LocalDate birthDate;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String gender;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
     
 }
