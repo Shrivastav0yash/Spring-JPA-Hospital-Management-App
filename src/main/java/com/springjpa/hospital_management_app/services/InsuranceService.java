3d88570bccc78b480276bb46a18a9072dd2b9642
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-29 23:22:08 +0530

    git commit -m "Create Insurance Service "

---
+++ src/main/java/com/springjpa/hospital_management_app/services/InsuranceService.java
@@ -0,0 +1,31 @@
+package com.springjpa.hospital_management_app.services;
+
+import com.springjpa.hospital_management_app.entity.Insurance;
+import com.springjpa.hospital_management_app.entity.Patient;
+import com.springjpa.hospital_management_app.repository.InsuranceRepository;
+import com.springjpa.hospital_management_app.repository.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InsuranceService {
+
+    private final InsuranceRepository insuranceRepository;
+    private final PatientRepository patientRepository;
+
+    @Transactional
+    public Patient assignInsuranceToPatient(Insurance insurance, Long patientId){
+        Patient patient = patientRepository.findById(patientId)
+                .orElseThrow(() -> new EntityNotFoundException("Patient not found with id: "+patientId));
+
+        patient.setInsurance(insurance);
+        insurance.setPatient(patient); // bidirectional consistency maintain
+
+        return patient;
+
+    }
+
+}
