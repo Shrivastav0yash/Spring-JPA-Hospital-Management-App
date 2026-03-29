e7c18c9eec54844b80bc56f500719fc477777f63
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-29 12:41:23 +0530

    git commit -m "create entity insurance"

---
+++ src/main/java/com/springjpa/hospital_management_app/entity/Insurance.java
@@ -0,0 +1,38 @@
+package com.springjpa.hospital_management_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String policyNumber;
+
+    @Column(nullable = false, length = 100)
+    private String provider;
+
+    @Column(nullable = false)
+    private LocalDate validUntil;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "insurance") //inverse side of JPA
+    private Patient patient;
+}
