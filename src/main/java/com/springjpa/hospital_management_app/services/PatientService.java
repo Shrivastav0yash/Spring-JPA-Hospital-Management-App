667e9e8a45f5b2a604faa3dcb158b8a07b941190
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-03 11:47:49 +0530

    git commit -m "Add API calls"

--- src/main/java/com/springjpa/hospital_management_app/services/PatientService.java
+++ src/main/java/com/springjpa/hospital_management_app/services/PatientService.java
@@ -1,22 +1,32 @@
 package com.springjpa.hospital_management_app.services;
 
+import com.springjpa.hospital_management_app.dto.PatientAppointmentResponseDTO;
+import com.springjpa.hospital_management_app.dto.PatientResponseDTO;
 import com.springjpa.hospital_management_app.entity.Patient;
 import com.springjpa.hospital_management_app.repository.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private final ModelMapper modelMapper;
 
-    @Transactional
-    public Patient getPatientById(Long id){
-        Patient p1 = patientRepository.findById(id).orElseThrow();
-        Patient p2 = patientRepository.findById(id).orElseThrow();
-        return p1;
+    public List<PatientResponseDTO> getAll(){
+        List<Patient> allPatients = patientRepository.findAll();
+        return allPatients.stream().map(patient -> modelMapper.map(patient, PatientResponseDTO.class)).toList();
     }
 
+    public PatientAppointmentResponseDTO getPatientById(Long patientId){
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new EntityNotFoundException("Patient Not " +
+                "Found with id: " + patientId));
+        return modelMapper.map(patient,PatientAppointmentResponseDTO.class);
+    }
 }
