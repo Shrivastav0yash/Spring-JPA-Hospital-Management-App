1557fe4464c88c68cac4646ffd9c64f7206d5403
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-03 12:53:51 +0530

    git commit -m "Comment"

--- src/test/java/com/springjpa/hospital_management_app/services/AppointmentServiceTest.java
+++ src/test/java/com/springjpa/hospital_management_app/services/AppointmentServiceTest.java
@@ -1,6 +1,7 @@
 package com.springjpa.hospital_management_app.services;
 
 import com.springjpa.hospital_management_app.entity.Appointment;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ public class AppointmentServiceTest {
     @Autowired
     private AppointmentService appointmentService;
 
+    @Disabled
     @Test
     public void testCreateAppointment(){
         Appointment appointment = Appointment.builder()
@@ -20,13 +22,13 @@ public class AppointmentServiceTest {
                 .reason("Cancer")
                 .build();
 
-        Appointment newAppointment = appointmentService.createNewAppointment(appointment, 1L, 2L);
+        //Appointment newAppointment = appointmentService.createNewAppointment(appointment, 1L, 2L);
 
-        System.out.println(newAppointment);
+        //System.out.println(newAppointment);
 
-        Appointment updatedAppointment = appointmentService.reAssignAppointmentToAnotherDoctor(newAppointment.getId(), 3L);
+//        Appointment updatedAppointment = appointmentService.reAssignAppointmentToAnotherDoctor(newAppointment.getId(), 3L);
 
-        System.out.println(updatedAppointment);
+  //      System.out.println(updatedAppointment);
 
     }
 
