ad07be96e2fa6678c23187c310c94eba68ebdc53
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-03 13:28:28 +0530

    git commit -m ""Doctor Controller"

--- src/main/java/com/springjpa/hospital_management_app/controller/DoctorController.java
+++ src/main/java/com/springjpa/hospital_management_app/controller/DoctorController.java
@@ -2,7 +2,6 @@ package com.springjpa.hospital_management_app.controller;
 
 import com.springjpa.hospital_management_app.dto.DoctorAppointmentsDTO;
 import com.springjpa.hospital_management_app.services.AppointmentService;
-import com.springjpa.hospital_management_app.services.DoctorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +14,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DoctorController {
 
-    private final DoctorService doctorService;
     private final AppointmentService appointmentService;
 
     @GetMapping("/{doctorId}/appointments")
