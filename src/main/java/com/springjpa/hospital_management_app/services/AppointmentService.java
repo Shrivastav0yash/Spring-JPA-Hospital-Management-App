c9bc36e2912afe5256c839abb3ed3f9a03fd4ef6
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-03 12:53:31 +0530

    git commit -m "Add method to get all appointments"

--- src/main/java/com/springjpa/hospital_management_app/services/AppointmentService.java
+++ src/main/java/com/springjpa/hospital_management_app/services/AppointmentService.java
@@ -1,5 +1,7 @@
 package com.springjpa.hospital_management_app.services;
 
+import com.springjpa.hospital_management_app.dto.CreateAppointmentDTO;
+import com.springjpa.hospital_management_app.dto.DoctorAppointmentsDTO;
 import com.springjpa.hospital_management_app.entity.Appointment;
 import com.springjpa.hospital_management_app.entity.Doctor;
 import com.springjpa.hospital_management_app.entity.Patient;
@@ -7,10 +9,13 @@ import com.springjpa.hospital_management_app.repository.AppointmentRepository;
 import com.springjpa.hospital_management_app.repository.DoctorRepository;
 import com.springjpa.hospital_management_app.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.print.Doc;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,20 +24,21 @@ public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final DoctorRepository doctorRepository;
     private final PatientRepository patientRepository;
+    private final ModelMapper modelMapper;
 
     @Transactional
-    public Appointment createNewAppointment(Appointment appointment, Long doctorId, Long patientId){
-        Doctor doctor = doctorRepository.findById(doctorId).orElseThrow();
-        Patient patient = patientRepository.findById(patientId).orElseThrow();
+    public void createNewAppointment(CreateAppointmentDTO appointmentDTO){
+        Doctor doctor = doctorRepository.findById(appointmentDTO.getDoctorId()).orElseThrow();
+        Patient patient = patientRepository.findById(appointmentDTO.getPatientId()).orElseThrow();
 
-        if(appointment.getId() != null) throw new IllegalArgumentException("Appointment should not have");
-
-        appointment.setPatient(patient);
+        Appointment appointment = new Appointment();
         appointment.setDoctor(doctor);
+        appointment.setPatient(patient);
+        appointment.setAppointmentTime(appointmentDTO.getAppointmentTime());
+        appointment.setReason(appointmentDTO.getReason());
 
-        patient.getAppointments().add(appointment);//maintain consistency
-
-        return appointmentRepository.save(appointment);
+        patient.getAppointments().add(appointment);
+        appointmentRepository.save(appointment);
     }
 
     @Transactional
@@ -43,5 +49,11 @@ public class AppointmentService {
         appointment.setDoctor(doctor); //this will automatically update because of dirty check
         doctor.getAppointments().add(appointment); //just for bidirectional consistency
         return appointment;
+    }
+
+    public List<DoctorAppointmentsDTO>  getAllAppointmentsOfDoctor(Long doctorId){
+        Doctor doctor = doctorRepository.findById(doctorId).orElseThrow();
+        List<Appointment> appointments = doctor.getAppointments();
+        return appointments.stream().map(appointment -> modelMapper.map(appointment, DoctorAppointmentsDTO.class)).toList();
     }
 }
