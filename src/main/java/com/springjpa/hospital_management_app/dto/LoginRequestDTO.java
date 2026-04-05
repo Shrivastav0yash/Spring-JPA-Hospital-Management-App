c5e40ec0ba5ab3fe27e68223b2414cf261e795ae
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-05 20:39:15 +0530

    git commit -m "Add Constructor"

--- src/main/java/com/springjpa/hospital_management_app/dto/LoginRequestDTO.java
+++ src/main/java/com/springjpa/hospital_management_app/dto/LoginRequestDTO.java
@@ -1,8 +1,13 @@
 package com.springjpa.hospital_management_app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
     private String username;
     private String password;
